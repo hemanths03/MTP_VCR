062424e8ccbf91089d63d96b5a3e97dc48d62a75
Hemanth <88610642+hemanths03@users.noreply.github.com>
2022-12-14 14:55:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem to work?!?
 
 # Change these to match where your annotations and images are
-VCR_IMAGES_DIR = os.path.join(os.path.dirname(__file__), 'data', 'vcr1images')
-VCR_ANNOTS_DIR = os.path.join(os.path.dirname(__file__), 'data')
+VCR_IMAGES_DIR = "/content/drive/MyDrive/MTP/data/vcr1images/"
+VCR_ANNOTS_DIR = "/content/drive/MyDrive/MTP/data"
 
 if not os.path.exists(VCR_IMAGES_DIR):
     raise ValueError("Update config.py with where you saved VCR images to.")
